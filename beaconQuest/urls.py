1e77756d4fe01405960fa5859e1da2e28c4cab91
DanailPenev <fcbdelrio@gmail.com>
2016-10-30 09:56:25 +0000

    Clearing up stuff

--- beaconQuest/urls.py
+++ beaconQuest/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^index/answerChallenge/(?P<accounts_id>[\w\-]+)/(?P<beacon_id>[\w\-]+)/(?P<answer>[\w\-]+)/$', views.challenge_answer, name='challenge_answer'),
     url(r'^index/cancelChallenge/(?P<accounts_id>[\w\-]+)/(?P<beacon_id>[\w\-]+)/$', views.challenge_cancel, name='challenge_cancel'),
     url(r'^index/register/(?P<username>[\w\-]+)/(?P<password>[\w\-]+)/$', views.register_account, name='register_account'),
-    url(r'^index/register/(?P<username>[\w\-]+)/(?P<password>[\w\-]+)/$', views.login_account, name='login_account'),
+    url(r'^index/login/(?P<username>[\w\-]+)/(?P<password>[\w\-]+)/$', views.login_account, name='login_account'),
 ]
